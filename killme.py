cfc749bde7270c288c1b6707032e26599c1a8020
Adrian Loh <adrianloh@gmail.com>
2013-12-04 20:12:15 +0800

    Cleanup

---
+++ killme.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os, re, time
+from urllib2 import urlopen
+import atexit
+
+instance_id = urlopen("http://169.254.169.254/latest/meta-data/instance-id").read()
+base = "https://badaboom.firebaseio-demo.com"
+
+cmd = "curl -s -m 2 http://169.254.169.254/latest/user-data | grep base="
+userbase = os.popen(cmd).read().strip()
+if userbase:
+	base = re.sub("badaboom", userbase.split("=")[1], base)
+
+@atexit.register
+def removeBase():
+	url = base + "/hqserver/" + instance_id + ".json"
+	cmd = "curl -X DELETE " + url
+	os.popen(cmd).read()
+
+while 1:
+	time.sleep(60)
